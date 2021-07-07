c77c7ba08ce951281719cb4fe106c74d94602109
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-07 11:31:27 +0300

    defined controllers

---
+++ internal/app/api/controller/event.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"net/http"
+)
+
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetEvent(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateEvent(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateEvent(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteEvent(w http.ResponseWriter, r *http.Request) {
+
+}
