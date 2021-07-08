f5b359b825efb7e8e23d59a33376c8c398ba05d9
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-08 16:16:44 +0300

    cleaned storage

--- storage/storage.go
+++ storage/storage.go
@@ -5,15 +5,8 @@ import (
 	"workshop2/internal/app/models"
 )
 
-var DB Storage
-
 type Storage struct {
 	Events        []models.Event
 	Notifications []models.Notification
 	sync.RWMutex
-}
-
-func init() {
-	DB.Events = make([]models.Event, 0)
-	DB.Notifications = make([]models.Notification, 0)
 }
