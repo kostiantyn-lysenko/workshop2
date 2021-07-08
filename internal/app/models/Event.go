a40feea332b90897bc6b948a33d431bef40551ed
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-08 12:14:47 +0300

    added struct's tags

--- internal/app/models/Event.go
+++ internal/app/models/Event.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Event struct {
-	ID          int
-	Title       string
-	Time        time.Time
-	Description string
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Time        time.Time `json:"time"`
+	Description string    `json:"description"`
 }
