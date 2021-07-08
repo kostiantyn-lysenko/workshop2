3c2d3a7f380bd01a7ff4eb7e9b6643f572089c4f
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-08 22:47:35 +0300

    implemented notif controller

--- internal/app/api/controller/notification.go
+++ internal/app/api/controller/notification.go
@@ -1,17 +1,76 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"workshop2/internal/app/errs"
+	"workshop2/internal/app/models"
+
+	"github.com/gorilla/mux"
 )
 
-func GetAllNotifications(w http.ResponseWriter, r *http.Request) {
-
+type NotificationServiceInterface interface {
+	GetAll() []models.Notification
+	Create(notification models.Notification) models.Notification
+	Update(id int, notification models.Notification) (models.Notification, error)
 }
 
-func CreateNotification(w http.ResponseWriter, r *http.Request) {
-
+type NotificationController struct {
+	Notifications NotificationServiceInterface
 }
 
-func UpdateNotification(w http.ResponseWriter, r *http.Request) {
+func (c *NotificationController) GetAll(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(c.Notifications.GetAll())
+}
 
+func (c *NotificationController) Create(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+
+	var notification models.Notification
+
+	err := json.NewDecoder(r.Body).Decode(&notification)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = &errs.FailedRequestParsingError{}
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	notification = c.Notifications.Create(notification)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(notification)
+}
+
+func (c *NotificationController) Update(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = &errs.IdNotNumericError{}
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	var notification models.Notification
+	err = json.NewDecoder(r.Body).Decode(&notification)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = &errs.FailedRequestParsingError{}
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	updatedEvent, err := c.Notifications.Update(id, notification)
+	if err != nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedEvent)
 }
