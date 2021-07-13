d30c2f4303e34fc5e8b431358500e33e634c2abd
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-13 16:38:15 +0300

    added errors

--- internal/app/errs/errs.go
+++ internal/app/errs/errs.go
@@ -23,3 +23,35 @@ type FailedRequestParsingError struct{}
 func (e *FailedRequestParsingError) Error() string {
 	return "Provided info is invalid."
 }
+
+type UserNotFoundError struct{}
+
+func (e *UserNotFoundError) Error() string {
+	return "User with that username does not exists in database."
+}
+
+func NewUserNotFoundError() error {
+	return &UserNotFoundError{}
+}
+
+type UserAlreadyExistsError struct{}
+
+func (e *UserAlreadyExistsError) Error() string {
+	return "User with that username already exists in database."
+}
+
+func NewUserAlreadyExistsError() error {
+	return &UserAlreadyExistsError{}
+}
+
+type UserValidationError struct {
+	Message string
+}
+
+func (e *UserValidationError) Error() string {
+	return e.Message
+}
+
+func NewUserValidationError(message string) error {
+	return &UserValidationError{Message: message}
+}
