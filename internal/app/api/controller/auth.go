93f1524247ce7277555172458808329c5f0e5915
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-15 19:07:33 +0300

    implemented authController

---
+++ internal/app/api/controller/auth.go
@@ -0,0 +1,68 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"workshop2/internal/app/errs"
+	"workshop2/internal/app/models"
+)
+
+type AuthServiceInterface interface {
+	SignUp(request models.SignUp) ([]models.Token, error)
+	SignIn(request models.SignIn) ([]models.Token, error)
+}
+
+type AuthController struct {
+	Auth AuthServiceInterface
+}
+
+func (c *AuthController) SignIn(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+
+	var signin models.SignIn
+
+	err := json.NewDecoder(r.Body).Decode(&signin)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = errs.NewFailedRequestParsingError()
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	tokens, err := c.Auth.SignIn(signin)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		err = errs.NewFailedAuthenticationError(err.Error())
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tokens)
+}
+
+func (c *AuthController) SignUp(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+
+	var signup models.SignUp
+
+	err := json.NewDecoder(r.Body).Decode(&signup)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = errs.NewFailedRequestParsingError()
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	tokens, err := c.Auth.SignUp(signup)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		err = errs.NewFailedSignUpError(err.Error())
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tokens)
+
+}
