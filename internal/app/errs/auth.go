b8f7d8df6d35bffe4c0b0ebb23c31280e5dff47d
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-15 19:07:43 +0300

    added errors

--- internal/app/errs/auth.go
+++ internal/app/errs/auth.go
@@ -11,3 +11,27 @@ func (e *AuthValidationError) Error() string {
 func NewAuthValidationError(message string) error {
 	return &AuthValidationError{Message: message}
 }
+
+type FailedAuthenticationError struct {
+	Message string
+}
+
+func (e *FailedAuthenticationError) Error() string {
+	return e.Message
+}
+
+func NewFailedAuthenticationError(message string) error {
+	return &FailedAuthenticationError{Message: message}
+}
+
+type FailedSignUpError struct {
+	Message string
+}
+
+func (e *FailedSignUpError) Error() string {
+	return e.Message
+}
+
+func NewFailedSignUpError(message string) error {
+	return &FailedSignUpError{Message: message}
+}
