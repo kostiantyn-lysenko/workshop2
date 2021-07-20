28c58784ed8525f84821360ca64f0297a8696c8d
kostiantyn-lysenko <klyse@softserveinc.com>
2021-07-20 12:27:36 +0300

    added update timezone method

--- internal/app/api/controller/user.go
+++ internal/app/api/controller/user.go
@@ -9,10 +9,12 @@ import (
 
 type UserServiceInterface interface {
 	Create(user models.User) (models.User, error)
+	UpdateTimezone(username string, timezone string) error
 }
 
 type UserController struct {
 	Users UserServiceInterface
+	Auth  AuthServiceInterface
 }
 
 func (e *UserController) Create(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +39,38 @@ func (e *UserController) Create(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(user)
+}
+
+func (c *UserController) UpdateTimezone(w http.ResponseWriter, r *http.Request) {
+	initHeaders(w)
+
+	var user models.User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		err = errs.NewFailedRequestParsingError()
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	tokenString, err := GetTokenCookie(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	claims, err := c.Auth.ExtractClaims(tokenString)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = c.Users.UpdateTimezone(claims.Subject, user.Timezone)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(err.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- internal/app/services/user.go
+++ internal/app/services/user.go
@@ -7,6 +7,7 @@ import (
 type UserRepositoryInterface interface {
 	Create(user models.User) (models.User, error)
 	Get(username string) (models.User, error)
+	Update(models.User) error
 }
 
 type UserService struct {
@@ -15,4 +16,15 @@ type UserService struct {
 
 func (s *UserService) Create(user models.User) (models.User, error) {
 	return s.Users.Create(user)
+}
+
+func (s *UserService) UpdateTimezone(username string, timezone string) error {
+	user, err := s.Users.Get(username)
+	if err != nil {
+		return err
+	}
+
+	user.Timezone = timezone
+
+	return s.Users.Update(user)
 }
