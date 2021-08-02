e98247f20304352089304452df6bd0912b463e01
kostiantyn-lysenko <klyse@softserveinc.com>
2021-08-02 17:21:36 +0300

    added json tags

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Username string `json:"username" validate:"required,min=3,max=40,alphanum,nefield=Password"`
-	Password string `json:"password" validate:"required,min=8"`
-	Timezone string `json:"timezone" validate:"required"`
+	Username string `db:"username" json:"username" validate:"required,min=3,max=40,alphanum,nefield=Password"`
+	Password string `db:"password_hash" json:"password" validate:"required,min=8"`
+	Timezone string `db:"timezone" json:"timezone" validate:"required"`
 }
